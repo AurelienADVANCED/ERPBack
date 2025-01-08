ad0410ca5ebbf911e5b5320e62415c5be16348d2
Aurelien ROSELLO <31188341+AurelienADVANCED@users.noreply.github.com>
2025-01-08 15:54:03 +0100

    Update WebConfig.java

--- src/main/java/com/estiam/aws/abar/erp/config/WebConfig.java
+++ src/main/java/com/estiam/aws/abar/erp/config/WebConfig.java
@@ -14,8 +14,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedHeaders("*");
             }
         };
     }
